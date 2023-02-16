945da83860148756d5203c5ff0dbdaa556ffedc4
han <hanpp123123@gmail.com>
2023-02-16 11:58:44 +0800

    before mark tag

--- test_slice.go
+++ test_slice.go
@@ -1,8 +1,18 @@
 package main
 
-func main() {
-	s1 := []int{11, 22, 33}
-	s2 := []int{44, 55, 66}
-	s1 = append(s1, s2...)
+import (
+	"fmt"
+)
 
+var testslice []int
+
+func main() {
+	for i := 1; i < 10; i++ {
+		sliceWrite(testslice)
+	}
+	fmt.Println(testslice)
+}
+
+func sliceWrite(s []int) {
+	s = append(s, 1)
 }
